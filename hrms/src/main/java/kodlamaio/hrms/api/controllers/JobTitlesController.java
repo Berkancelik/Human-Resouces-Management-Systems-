d01fdae48734d1dacab945cfb30a2c951d8f984b
Berkan Çelik <Berkan elik@DESKTOP-UFSIJMT>
2021-05-23 19:01:32 +0300

    new commit

--- hrms/src/main/java/kodlamaio/hrms/api/controllers/JobTitlesController.java
+++ hrms/src/main/java/kodlamaio/hrms/api/controllers/JobTitlesController.java
@@ -2,6 +2,7 @@ package kodlamaio.hrms.api.controllers;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +13,7 @@ import kodlamaio.hrms.entities.concretes.JobTitle;
 @RestController
 @RequestMapping("/api/jobtitles")
 public class JobTitlesController {
-	
+	@Autowired
 	private JobTitleService jobTitleService;
 	
 	public JobTitlesController(JobTitleService jobTitleService) {
