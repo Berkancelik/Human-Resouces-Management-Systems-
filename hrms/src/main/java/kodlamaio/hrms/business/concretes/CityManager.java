0ba0b5c98f8d189e9bfd2971411768eb67c3e26b
Berkan Çelik <Berkan elik@DESKTOP-UFSIJMT>
2021-05-29 18:31:44 +0300

    citiesController, CityDao,CityManager,CityService has created

---
+++ hrms/src/main/java/kodlamaio/hrms/business/concretes/CityManager.java
@@ -0,0 +1,28 @@
+package kodlamaio.hrms.business.concretes;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import kodlamaio.hrms.business.abstracts.CityService;
+import kodlamaio.hrms.core.utilities.results.DataResult;
+import kodlamaio.hrms.core.utilities.results.SuccessDataResult;
+import kodlamaio.hrms.dataAccess.abstracts.CityDao;
+import kodlamaio.hrms.entities.concretes.City;
+
+public class CityManager implements CityService {
+	
+	private CityDao cityDao;
+	
+	@Autowired
+	public  CityManager(CityDao cityDao) {
+		super();
+		this.cityDao = cityDao;
+	}
+
+	@Override
+	public DataResult<List<City>> getall() {
+		return new SuccessDataResult<List<City>>(this.cityDao.findAll());
+	}
+
+}
