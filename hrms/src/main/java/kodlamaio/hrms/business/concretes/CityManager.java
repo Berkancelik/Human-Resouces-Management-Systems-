68f4468dd2d47e8a79315fa91edcc3bf9431ff5a
Berkan Çelik <Berkan elik@DESKTOP-UFSIJMT>
2021-05-30 14:21:45 +0300

    @Service bug fix

--- hrms/src/main/java/kodlamaio/hrms/business/concretes/CityManager.java
+++ hrms/src/main/java/kodlamaio/hrms/business/concretes/CityManager.java
@@ -3,13 +3,14 @@ package kodlamaio.hrms.business.concretes;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import kodlamaio.hrms.business.abstracts.CityService;
 import kodlamaio.hrms.core.utilities.results.DataResult;
 import kodlamaio.hrms.core.utilities.results.SuccessDataResult;
 import kodlamaio.hrms.dataAccess.abstracts.CityDao;
 import kodlamaio.hrms.entities.concretes.City;
-
+@Service
 public class CityManager implements CityService {
 	
 	private CityDao cityDao;
--- hrms/src/main/java/kodlamaio/hrms/business/concretes/EmployeeManager.java
+++ hrms/src/main/java/kodlamaio/hrms/business/concretes/EmployeeManager.java
@@ -3,13 +3,14 @@ package kodlamaio.hrms.business.concretes;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import kodlamaio.hrms.business.abstracts.EmployeeService;
 import kodlamaio.hrms.core.utilities.results.DataResult;
 import kodlamaio.hrms.core.utilities.results.SuccessDataResult;
 import kodlamaio.hrms.dataAccess.abstracts.EmployeeDao;
 import kodlamaio.hrms.entities.concretes.Employee;
-
+@Service
 public class EmployeeManager implements EmployeeService{
 	private EmployeeDao employeeDao;
 
