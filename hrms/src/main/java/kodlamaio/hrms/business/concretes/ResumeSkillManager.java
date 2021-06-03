ac5e9634c4f1ada8551f26ba351200d24cf66501
Berkan Çelik <Berkan elik@DESKTOP-UFSIJMT>
2021-06-03 11:12:42 +0300

    ResumeSkill api has created

---
+++ hrms/src/main/java/kodlamaio/hrms/business/concretes/ResumeSkillManager.java
@@ -0,0 +1,62 @@
+package kodlamaio.hrms.business.concretes;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kodlamaio.hrms.business.abstracts.ResumeSkillService;
+import kodlamaio.hrms.core.utilities.results.DataResult;
+import kodlamaio.hrms.core.utilities.results.Result;
+import kodlamaio.hrms.core.utilities.results.SuccessDataResult;
+import kodlamaio.hrms.core.utilities.results.SuccessResult;
+import kodlamaio.hrms.dataAccess.abstracts.ResumeSkillDao;
+import kodlamaio.hrms.entities.concretes.ResumeSkill;
+@Service
+public class ResumeSkillManager implements ResumeSkillService {
+	private ResumeSkillDao resumeSkillDao;
+	
+	@Autowired
+	public ResumeSkillManager(ResumeSkillDao resumeSkillDao) {
+		super();
+		this.resumeSkillDao = resumeSkillDao;
+	}
+
+	@Override
+	public Result add(ResumeSkill resumeSkill) {
+		this.resumeSkillDao.save(resumeSkill);
+		return new SuccessResult("Yetenek eklendi!");
+
+	}
+
+	@Override
+	public Result update(ResumeSkill resumeSkill) {
+		this.resumeSkillDao.save(resumeSkill);
+		return new SuccessResult("Yetenek güncellendi!");
+	}
+
+	@Override
+	public Result delete(int id) {
+		this.resumeSkillDao.deleteById(id);
+		return new SuccessResult("Yetenek silindi!");
+	}
+
+	@Override
+	public DataResult<ResumeSkill> getById(int id) {
+		return new SuccessDataResult<ResumeSkill>(this.resumeSkillDao.getById(id));
+		
+	}
+
+	@Override
+	public DataResult<List<ResumeSkill>> getAll() {
+		return new SuccessDataResult<List<ResumeSkill>>(this.resumeSkillDao.findAll());
+
+	}
+
+	@Override
+	public DataResult<List<ResumeSkill>> getAllByCandidateId(int id) {
+		return new SuccessDataResult<List<ResumeSkill>>(this.resumeSkillDao.getAllByCandidateId(id));
+		
+	}
+
+}
