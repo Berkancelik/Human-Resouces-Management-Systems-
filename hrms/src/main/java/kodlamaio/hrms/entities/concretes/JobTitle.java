6de911f34cfcbc3d604e97a3c98ac9feebcbdec9
Berkan Çelik <Berkan elik@DESKTOP-UFSIJMT>
2021-06-04 23:16:37 +0300

    some fix bug

--- hrms/src/main/java/kodlamaio/hrms/entities/concretes/JobTitle.java
+++ hrms/src/main/java/kodlamaio/hrms/entities/concretes/JobTitle.java
@@ -10,13 +10,11 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
-import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Entity
