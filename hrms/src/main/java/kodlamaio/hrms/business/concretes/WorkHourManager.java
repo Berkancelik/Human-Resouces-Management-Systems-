d21ac75b376d5122b81502aec5e66d4c31eb199c
Berkancelik <celikberkan1@outlook.com>
2021-06-17 13:11:35 +0300

    WokHour, WokHourDao, WokHourService, WokHourManager, WokHourController has been created

---
+++ hrms/src/main/java/kodlamaio/hrms/business/concretes/WorkHourManager.java
@@ -0,0 +1,39 @@
+package kodlamaio.hrms.business.concretes;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kodlamaio.hrms.business.abstracts.WorkHourService;
+import kodlamaio.hrms.core.utilities.results.DataResult;
+import kodlamaio.hrms.core.utilities.results.Result;
+import kodlamaio.hrms.core.utilities.results.SuccessDataResult;
+import kodlamaio.hrms.core.utilities.results.SuccessResult;
+import kodlamaio.hrms.dataAccess.abstracts.WorkHourDao;
+import kodlamaio.hrms.entities.concretes.WorkHour;
+
+@Service
+public class WorkHourManager implements WorkHourService {
+
+	private WorkHourDao workHourDao;
+	
+	
+	@Autowired
+	public WorkHourManager(WorkHourDao workHourDao) {
+		super();
+		this.workHourDao = workHourDao;
+	}
+
+	@Override
+	public Result add(WorkHour workHour) {
+		this.workHourDao.save(workHour);
+		return new SuccessResult("job Seeker Added");
+	}
+
+	@Override
+	public DataResult<List<WorkHour>> getAll() {
+		return new SuccessDataResult<List<WorkHour>>(this.workHourDao.findAll());
+	}
+
+}
